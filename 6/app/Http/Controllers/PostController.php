df7bf030073d2831fbcdaae9a67366661da00688
Lorfy2017 <katy1031katy@outlook.com>
2022-12-21 16:27:17 +0900

    6章チェックテスト

---
+++ 6/app/Http/Controllers/PostController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Broadcasting\Broadcasters\UsePusherChannelConventions;
+use Illuminate\Http\Request;
+use App\Post;
+use App\User;
+
+class PostController extends Controller
+{
+    public function add()
+    {
+        return view('post');
+    }
+  public function create(Request $request)
+  {
+    $this->validate($request, Post::$rules);
+    $post = new Post;
+    $form = $request->all();
+    unset($form['_token']);
+
+    // データベースに保存
+    $post->fill($form);
+    $post->save();
+
+    return redirect('/post');
+  }
+
+  public function index()
+  {
+    $user = User::all();
+    $post = Post::orderBy('id', 'desc')->get();
+      return view('post',['users'=>$user,'posts'=>$post]);
+  }
+
+  public function delete(Request $request)
+  {
+    $post = Post::find($request->id);
+        $post->delete();
+
+        return redirect('/post');
+  }
+
+
+}
---
+++ 6/resources/views/post.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+@section('title', 'つぶやき一覧')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto w-75">
+            <div class="card w-75 border-secondary">
+                <form action="{{ action('PostController@create') }}" method="post" enctype="multipart/form-data">
+
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group w-75" >
+                        <label class="col-md-10 lead" for="body">なんでもつぶやいちゃえ！
+                        </label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" rows="1" name="body" placeholder="いまなにしてる？">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+
+                    {{ csrf_field() }}
+                    <div class = "text-right">
+                    <input type="submit" class="btn btn-primary" value="つぶやく">
+                    <input type="hidden" name="user_id" value = "{{Auth::user()->id}}">
+                </form></div></div><br><br>
+
+
+<div class = "card w-75 border-secondary mb-3 d-flex justify-content-center" >
+    @foreach($posts as $post)
+    @foreach($users as $user)
+    @if($user -> id == $post -> user_id)
+
+
+
+
+
+    
+   
+    <div class ="card-header">
+        <div class="pull-left mr-auto">{{$user->name}}さん</div>
+        <div class="text-right">{{$post->created_at}}</div>
+    </div>
+                           
+    
+        
+    
+    <div class = "card-body">{{$post->body}}</div>
+
+    @if(Auth::id() == $post->user_id)
+    <div class="text-right">
+    <a class = "btn btn-danger" href="{{ action('PostController@delete', ['id' => $post->id]) }}">削除</a>
+    </div>
+    @endif
+    @endif
+    @endforeach
+    @endforeach
+    
+</div>
+
+
+
+
+
+            </div>
+        </div>
+    </div>
+@endsection
