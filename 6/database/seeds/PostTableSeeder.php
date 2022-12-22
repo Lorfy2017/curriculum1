e137dbc9ed0c5ae35e0514436f83433a89433bc3
Lorfy2017 <katy1031katy@outlook.com>
2022-12-22 22:31:41 +0900

    6章修正

---
+++ 6/database/seeds/PostTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Post;
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 8; $i <= 10; $i++) {
+            DB::table('posts')->insert([
+                'user_id' => $i,
+                'body' => 'これはテスト!'.$i,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
